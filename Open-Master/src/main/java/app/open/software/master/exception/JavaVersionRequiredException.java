fb5a7601b0b233a06d31e964697525822aab72bb
Tammo0987 <tammo.steffens@googlemail.com>
2018-10-06 18:41:58 +0200

    Implemented JavaVersionRequiredException

---
+++ Open-Master/src/main/java/app/open/software/master/exception/JavaVersionRequiredException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2018, Open-Software and contributors
+ *
+ * The code is licensed under the MIT License, which can be found in the root directory of the repository
+ */
+
+package app.open.software.master.exception;
+
+/**
+ * This {@link Exception} should be thrown, if the required Java version is not installed on the current machine
+ *
+ * @author Tammo0987
+ * @since 0.1
+ */
+public class JavaVersionRequiredException extends Exception{
+
+	/**
+	 * Create a new {@link JavaVersionRequiredException} with the default message
+	 */
+	public JavaVersionRequiredException() {
+		super("Java 11 is required to start the Open-Cloud!");
+	}
+
+}
