d3e1e777a4e690901f90185176f87bb468f6b470
Tammo0987 <22091805+Tammo0987@users.noreply.github.com>
2018-10-09 16:14:10 +0200

    Added Copyright

--- Open-Container/src/main/java/app/open/software/container/bootstrap/ContainerBootstrap.java
+++ Open-Container/src/main/java/app/open/software/container/bootstrap/ContainerBootstrap.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018, Open-Software and contributors
+ *
+ * The code is licensed under the MIT License, which can be found in the root directory of the repository
+ */
+
 package app.open.software.container.bootstrap;
 
 import app.open.software.container.Container;
