21ea2e76153ec88b618e9aa02fda4db53c3522f7
Tammo0987 <tammo.steffens@googlemail.com>
2019-01-05 22:50:41 +0100

    Added logback configuration

--- Open-Rest/src/main/kotlin/app/open/software/rest/WebServer.kt
+++ Open-Rest/src/main/kotlin/app/open/software/rest/WebServer.kt
@@ -1,17 +1,26 @@
 package app.open.software.rest
 
+import com.google.gson.GsonBuilder
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.Compression
+import io.ktor.features.DefaultHeaders
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.netty.NettyApplicationEngine
 import java.util.concurrent.TimeUnit
 
-class WebServer (private val port: Int){
+class WebServer(private val port: Int) {
+
+    private val gson = GsonBuilder().setPrettyPrinting().create()
 
     private var server: NettyApplicationEngine? = null
 
     init {
         this.server = embeddedServer(Netty, this.port) {
-
+            install(DefaultHeaders)
+            install(Compression)
+            install(CallLogging)
         }
     }
 
@@ -20,6 +29,7 @@ class WebServer (private val port: Int){
     }
 
     fun stop() {
+        //TODO set time values
         this.server?.stop(Long.MAX_VALUE, 0, TimeUnit.MILLISECONDS)
     }
 
